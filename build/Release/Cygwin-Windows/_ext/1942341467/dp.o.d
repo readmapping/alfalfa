9dc77c27bda7489d941dbe87d703d0fd2632a79c
Nico Van Cleemput <nvcleemp@smeik.ugent.be>
2012-06-01 09:53:51 +0200

    Maybe best to not version the build and dist directory

--- build/Release/Cygwin-Windows/_ext/1942341467/dp.o.d
+++
@@ -1,5 +0,0 @@
-build/Release/Cygwin-Windows/_ext/1942341467/dp.o: \
- /cygdrive/C/Users/mvyvermn/Documents/NetBeansProjects/alfalfa/dp.cpp \
- /cygdrive/C/Users/mvyvermn/Documents/NetBeansProjects/alfalfa/dp.h
-
-/cygdrive/C/Users/mvyvermn/Documents/NetBeansProjects/alfalfa/dp.h:
--- build/Release/Cygwin-Windows/_ext/1942341467/qsufsort.o.d
+++
@@ -1,2 +0,0 @@
-build/Release/Cygwin-Windows/_ext/1942341467/qsufsort.o: \
- /cygdrive/C/Users/mvyvermn/Documents/NetBeansProjects/alfalfa/qsufsort.c
